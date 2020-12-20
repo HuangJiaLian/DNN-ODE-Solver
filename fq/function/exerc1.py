ae58e5d81b1850b6907570270da465cb858e068d
fangqian <1433667664@qq.com>
2020-12-20 16:26:49 +0800

    add testing code

---
+++ fq/function/exerc1.py
@@ -0,0 +1,13 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+t=np.linspace(0,30)
+T=-15*np.exp(-0.2*t)+20
+plt.title('Function')
+plt.xlabel('t')
+plt.ylabel('T')
+plt.grid()
+plt.plot(t,T,label='$T= e^{-0.2t}+20$')
+plt.legend()
+plt.savefig('function.png')
+plt.savefig('function.eps')
+plt.show()
